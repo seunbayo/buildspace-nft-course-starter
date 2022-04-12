28eb5bd5fd38b78d05edaaafe43874f1fb28d4b3
seunbayo <seunbayo83@gmail.com>
2022-04-12 09:53:55 +0100

    added contract to connect metamask

--- src/App.js
+++ src/App.js
@@ -3,26 +3,67 @@ import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'SeunbayoNg';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
-const checkIfWalletIsConnected = () => {
-  // first make sure we have access to window.ethereum
-  const { ethereum } = window;
-  if (!ethereum) {
-    console.log('Make sure you have metamask!');
-    return;
-  } else {
-    console.log('We have the ethereum object', ethereum);
-  }
-}
-
 const App = () => {
+
+  /* Just a state variable we use to store our user''s public wallet. Dont forget to import useState */
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  /* To make sure its async */
+  const checkIfWalletIsConnected = async () => {
+    // first make sure we have access to window.ethereum
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+      return;
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+
+    /* check if we are authorized to access the users wallet */
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    /* User can have multiple authorized accounts, we grab the first one if its there! */
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  // Implementing currentWallet Method here
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      // Fancy method to request access to account.
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
@@ -31,6 +72,10 @@ const App = () => {
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
+
+  /*
+  * Added a conditional render! We don't want to show Connect to Wallet if we're already connected :).
+  */
 
 
   return (
@@ -41,7 +86,13 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+              <button onClick={null} className="cta-button connect-wallet-button">
+                Mint NFT
+              </button>
+            )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -50,11 +101,12 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE} via buildspace`}</a>
         </div>
       </div>
     </div>
   );
+
 };
 
 export default App;
